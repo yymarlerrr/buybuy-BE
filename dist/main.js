db88f368203edbb2b79dea0d7cbccba6afa66a5e
yymarlerrr <yymarlerr@gmail.com>
2025-03-22 20:45:29 +0800

    fix: cors

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,14 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: [
+            'http://localhost:5173',
+            'https://4eab-123-192-236-27.ngrok-free.app',
+            'https://buybuy-fe-production.up.railway.app',
+            'https://liff.line.me/2007085677-AY2mR3rN'
+        ]
+    });
     await app.listen(process.env.PORT ?? 3000);
     console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+    const app = await NestFactory.create(AppModule)
+    app.enableCors({
+        origin: [
+            'http://localhost:5173',
+            'https://4eab-123-192-236-27.ngrok-free.app',
+            'https://buybuy-fe-production.up.railway.app',
+            'https://liff.line.me/2007085677-AY2mR3rN'
+        ]
+    })
+    await app.listen(process.env.PORT ?? 3000)
+    console.log(`Server is running on port ${process.env.PORT ?? 3000}`)
 }
-bootstrap();
+bootstrap()
